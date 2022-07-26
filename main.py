077aace5a20d1f97c3559f6a917bca67633238a8
Matt3o0 <duvbolone@gmail.com>
2022-07-26 13:01:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,25 @@ async def on_message(message):
     if message.content == "When release":
         await message.channel.send("When it's done... Read the FAQ before asking questions please!")
     else:
-        if message.content == "will 3D cabin":
-            await message.channel.send("Yes! But read the FAQ before asking questions please!")
-        else: 
-            if message.content == "will custom FMC":
-                await messsage.channel.send("Most likely, read the FAQ before asking these questions tho!")
-            else:
-                return
+        return
+
+@bot.listen()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content == "will 3D cabin":
+        await message.channel.send("Yes! But read the FAQ before asking questions please!")
+    else: 
+        return
+        
+@bot.listen()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content == "will custom FMC":
+        await messsage.channel.send("Most likely, read the FAQ before asking these questions tho!")
+    else:
+        return
 
 
 @bot.command(name="echo",description="Send a message as the bot.")
