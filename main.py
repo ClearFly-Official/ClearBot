14ceac3f7def62c9d5d5fa033700941d6b4ba135
Matt3o0 <duvbolone@gmail.com>
2022-07-26 10:18:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ client = discord.Client()
 async def on_ready():
     print("I'm ready for usage!")
 
+@bot.event 
+async def on_member_join(member):
+     channel=bot.get_channel(1001401783689678868)
+     emb=discord.Embed(description=f"Thanks {member.mention} for joining!")
+     emb.set_image(url=userAvatarUrl)
+     await channel.send(embed=emb)
+
 bot.run(os.environ['TOKEN'])
