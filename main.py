ff72ce4a3910d90af90309f2dd6331f8610fb018
Matt3o0 <duvbolone@gmail.com>
2022-07-26 09:51:58 +0200

    added required files

---
+++ main.py
@@ -0,0 +1,10 @@
+########################
+#-Made by Matt3o0#4764-#
+########################
+import discord
+import os
+
+bot = discord.Bot()
+client = discord.Client()
+
+bot.run(os.environ['TOKEN'])
