6f54fa41a2f015ffa604b9cc7ac719ea3f8f9049
Matt3o0 <duvbolone@gmail.com>
2022-07-29 21:33:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def on_raw_reaction_add(payload):
   print("suss")
 
 @bot.listen()
-async def on_message_delete(message):
+async def on_raw_message_delete(message, payload):
   channel = bot.get_channel(1001405648828891187)
   msgdel = message.clean_content
   msgatr = message.author.mention
