4ce9f35d33812e61b37d17f213df91cdac733b38
Matt3o0 <duvbolone@gmail.com>
2022-07-30 15:16:46 +0200

    e
    e

--- main.py
+++ main.py
@@ -177,8 +177,4 @@ Members: {members}
 
 #############################################
 
-for filename in os.listdir('./cogs'):
-  if filename.endswith('.py'):
-    bot.load_extension(f'cogs.{filename[:-3]}')
-
 bot.run(os.environ['TOKEN'])
