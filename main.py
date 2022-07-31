e0169e73ddefe4a71d36d849f8a539508b62cc85
Matt3o0 <duvbolone@gmail.com>
2022-07-31 10:20:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,20 +59,6 @@ async def on_message_delete(message):
   await channel.send(embed=emb)
 
 @bot.listen()
-async def on_raw_message_delete(message, payload):
-  channel = bot.get_channel(1001405648828891187)
-  msgdel = message.clean_content
-  msgatr = message.author.mention
-  msgcnl = message.channel.mention
-  pfp = message.author.avatar.url
-  emb = discord.Embed(title="**Message Deleted:**", color=0x4f93cf)
-  emb.add_field(name="Content:", value=f"{msgdel}", inline = False)
-  emb.add_field(name="Author:", value=f"{msgatr}", inline = True)
-  emb.add_field(name="Channel:", value=f"{msgcnl}", inline = True)
-  emb.set_thumbnail(url=pfp)
-  await channel.send(embed=emb)
-
-@bot.listen()
 async def on_message_edit(before, after):
   channel = bot.get_channel(1001405648828891187)
   msgeditb = before.clean_content
