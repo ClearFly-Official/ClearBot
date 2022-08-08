bfa88e027f7b2ef7271350a7eff5bf9e434d415d
Matt3o0 <duvbolone@gmail.com>
2022-08-08 11:53:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ async def on_ready():
 async def on_message(message):
   if message.channel.id == 1001401783689678868:
     await message.channel.send("<@&1001457701022343181> ^")
+    continue
   else:
     return
 @bot.listen()
 async def on_member_join(member):
     channel = bot.get_channel(965600413376200726)
-    memberid = member.id
     emb = discord.Embed(title=f"Welcome to ClearFly!", description=f"Hey there, {member.mention}! Be sure to read the <#1002194493304479784> to become a member and gain full access to the server! Thanks for joining!", color = 0x57a4cd)
     await channel.send(embed=emb)
 
