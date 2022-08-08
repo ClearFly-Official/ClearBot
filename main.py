1fc8afc6eb7362235c3a0d3cb21238af45f67b8e
Matt3o0 <duvbolone@gmail.com>
2022-08-08 10:46:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def on_ready():
 @bot.listen()
 async def on_message(message):
   if message.channel.id == 1001401783689678868:
-    await message.channel.send("<@965422406036488282> ^")
+    await message.channel.respond("<@965422406036488282> ^")
   else:
     return
 @bot.listen()
