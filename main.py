f8beccad49633ebc09bcee4261d4ede1af18d641
Matt3o0 <duvbolone@gmail.com>
2022-08-17 00:44:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -194,6 +194,10 @@ async def test(ctx):
   button1 = Button(label="I have read and accept the rules", style=discord.ButtonStyle.secondary, emoji="<:ClearFly_half_clear:1009117524677369866>")
 
   async def button_callback(interaction):
+    author = interaction.author
+    guild = discord.guildget_guild(965419296937365514)
+    role = guild.get_role(1009130013729235015)
+    await interaction.response.author.add_roles(role)
     await interaction.response.send_message("Rules accepted, have fun in the server!",ephemeral=True)
   
   button1.callback = button_callback
