82efad6ef1eb33ea39767f72a7e8da635b015a6d
Matt3o0 <duvbolone@gmail.com>
2022-08-17 01:13:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ class MyView(discord.ui.View):
 
     @discord.ui.button(label="I have read and accept the rules", custom_id="rulebutton", style=discord.ButtonStyle.secondary, emoji="<:ClearFly_half_clear:1009117524677369866>")
     async def button_callback(self, button, interaction):
-      author = interaction.author
+      author = interaction.user
       guild = discord.guild.get_guild(965419296937365514)
       role = guild.get_role(1009130013729235015)
       await interaction.author.add_roles(role)
