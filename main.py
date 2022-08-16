d49e1963f2e4b5693a871a271d299c690b47ca1a
Matt3o0 <duvbolone@gmail.com>
2022-08-17 01:23:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def on_message_edit(before, after):
 
 
 @bot.command(name="echo",description="Send a message as the bot.(Admin only)")
-@commands.has_role(965422406036488282)
+@commands.has_role(1006725140933001246)
 async def echo(ctx, text):
     await ctx.respond('posted your message!',ephemeral  = True)
     await ctx.channel.send(text)
@@ -93,7 +93,7 @@ async def echo(ctx, text):
     print(ctx.author, "used echo:", text)
 
 @bot.command(name="embed",description="Send an embed as the bot.(Admin only)")
-@commands.has_role(965422406036488282)
+@commands.has_role(1006725140933001246)
 async def embed(ctx, title, description):
     await ctx.respond('posted your embed!',ephemeral  = True)
     emb = discord.Embed(title=title, description=description, color=0x4f93cf)
@@ -197,15 +197,16 @@ class MyView(discord.ui.View):
     async def button_callback(self, button, interaction):
       author = interaction.user
       guild = bot.get_guild(965419296937365514)
-      role = guild.get_role(1009130013729235015)
+      role = guild.get_role(1002200398905483285)
       await author.add_roles(role)
       await interaction.response.send_message("Rules accepted, have fun in the server!",ephemeral=True)
 
 @bot.command(name="rules", descritpion="sends the rules(admin only)")
+@command.has_role(1006725140933001246)
 async def rules(ctx):
   embed = discord.Embed(title="ClearFly Rules", description="1. Don’t post any NSFW or inappropriate content. This will result in a warning, or an immediate ban depending on the severity.\n\n2. Post content in the correct channels.\n\n3. Do not spam, except in the spam channel.\n\n4. No harassment. If you are being harassed, let the staff know, and we will deal with it from there. Refrain from communicating with the person harassing you as we resolve the problem.\n\n5. Don’t excessively ping members. This will result in a mute.\n\n6. Don’t post any political content.\n\n7. Use common sense.\n\n8. Follow the [Discord TOS](https://discord.com/terms) and [Community Guidelines.](https://discord.com/guidelines)", color=cfc)
   await ctx.respond("rules posted!",ephemeral=True)
-  await ctx.send(f"{MyView.is_persistent(MyView())}", embed=embed,view=MyView())
+  await ctx.send(f"_ _{MyView.is_persistent(MyView())}", embed=embed,view=MyView())
 
 ##############################
 ##no more commands down here##
