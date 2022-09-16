fa7d373347484097ce2dc08a3aba44fb4883a2be
Matt3o0 <duvbolone@gmail.com>
2022-09-17 00:49:42 +0200

    sus

--- main.py
+++ main.py
@@ -127,26 +127,7 @@ async def on_message_edit(before, after):
   else:
     pass
 
-@bot.listen()
-async def on_message(message):
-  if message.channel.id == 966077223260004402:
-    start="0"
-    if message.content == "<@1001249135774666823> convo":
-      start="1"
-      await message.channel.send(f"Hey {message.author.mention}, how are you doing?")
-    if "good"in message.content:
-        start="2"
-        responses=[
-          "Nice to hear that.",
-          "Nice!",
-          "Glad you are doing fine!"
-        ]
-        await message.channel.send(f"{random.choice(responses)}")
-    if "how you" in message.content:
-      start="3"
-      await message.channel.send("I'm doing just fine, thanks for asking!")
-  else:
-    pass
+
 
 
 
@@ -245,7 +226,7 @@ async def embed(ctx, title: str, description: str):
     embed.set_thumbnail(url=pfp)
     await channel2.send(embed=embed)
 
-@bot.command(name='the-team', description='Shows The ClearFly Team!')
+@utility.command(name='the-team', description='Shows The ClearFly Team!')
 async def team(ctx):
   emb = discord.Embed(title="**The ClearFly Team:**",color=cfc)
   logo = "https://cdn.discordapp.com/attachments/927609657655177238/992887468410024026/ClearFly_Logo.png"
@@ -497,200 +478,217 @@ airports = [
 @option("origin", description="The airport(ICAO) you will fly from.", choices=airports)
 @option("destination", description="The airport(ICAO) you will fly to.", choices=airports)
 async def file(ctx, aircraft, origin, destination):
-  guild = bot.get_guild(965419296937365514)
-  cfpilot = guild.get_role(1013933799777783849)
-  if cfpilot in ctx.author.roles:
-    dest = 1
-    ori = 1
-    if dest == 1:
-      if destination == "KDCA":
-        cf1 = 1
-      if destination == "KIAD":
-        cf1 = 2
-      if destination == "KLGA":
-        cf1 = 3
-      if destination == "KMSP":
-        cf1 = 4
-      if destination == "KORD":
-        cf1 = 5
-      if destination == "KMDW":
-        cf1 = 6
-      if destination == "KMKE":
-        cf1 = 7
-      if destination == "KSFO":
-        cf1 = 8
-      if destination == "KLAX":
-        cf1 = 9
-      if destination == "KPHX":
-        cf1 = 10
-      if destination == "KSEA":
-        cf1 = 11
-      if destination == "KPDX":
-        cf1 = 12
-      if destination == "KRIC":
-        cf1 = 13
-      if destination == "KMIA":
-        cf1 = 14
-      if destination == "KSTL":
-        cf1 = 15
-      if destination == "KBOS":
-        cf1 = 16
-      if destination == "KIND":
-        cf1 = 17
-      if destination == "KIAH":
-        cf1 = 18
-      if destination == "KAUS":
-        cf1 = 19
-      if destination == "KDFW":
-        cf1 = 20
-      if destination == "KPIT":
-        cf1 = 21
-      if destination == "KATL":
-        cf1 = 22
-      if destination == "KSAN":
-        cf1 = 23
-    if ori == 1:
-      if origin == "KDCA":
-        cf2 = 1
-      if origin == "KIAD":
-        cf2 = 2
-      if origin == "KLGA":
-        cf2 = 3
-      if origin == "KMSP":
-        cf2 = 4
-      if origin == "KORD":
-        cf2 = 5
-      if origin == "KMDW":
-        cf2 = 6
-      if origin == "KMKE":
-        cf2 = 7
-      if origin == "KSFO":
-        cf2 = 8
-      if origin == "KLAX":
-        cf2 = 9
-      if origin == "KPHX":
-        cf2 = 10
-      if origin == "KSEA":
-        cf2 = 11
-      if origin == "KPDX":
-        cf2 = 12
-      if origin == "KRIC":
-        cf2 = 13
-      if origin == "KMIA":
-        cf2 = 14
-      if origin == "KSTL":
-        cf2 = 15
-      if origin == "KBOS":
-        cf2 = 16
-      if origin == "KIND":
-        cf2 = 17
-      if origin == "KIAH":
-        cf2 = 18
-      if origin == "KAUS":
-        cf2 = 19
-      if origin == "KDFW":
-        cf2 = 20
-      if origin == "KPIT":
-        cf2 = 21
-      if origin == "KSAN":
-        cf2 = 22
-      if origin == "KATL":
-        cf2 = 22
-    user = ctx.author
-    await ctx.respond("Filing flight.")
-    sleep(0.1)
-    await ctx.edit(content="Filing flight..")
-    embed = discord.Embed(title="Flight Filed!", color=cfc)
-    flightnumber = cf1+cf2
-    embed.add_field(name="Your flight has been filed with the following data:", value=f"""
-    ```
-    Aircraft:{aircraft}
-    Departure:{origin}
-    Arrival:{destination}
-    Flight Number: CF{flightnumber}
-    ```
-    Have a nice and safe flight!
-              """)
-    await ctx.edit(content="Filing flight...")
-    await ctx.edit(content="Filing flight.")
-    if os.path.exists(f"ClearFly_VA/users/{user.id}"):
-        f = open(f"ClearFly_VA/users/{user.id}/data.txt","a")
-        f.write(f"\nCF{flightnumber}, {aircraft}, {origin}-{destination}")
-        f.close()
-    else:
-        os.mkdir(f"ClearFly_VA/users/{user.id}")
-        f = open(f"ClearFly_VA/users/{user.id}/data.txt","a")
-        f.write(f"\nCF{flightnumber}, {aircraft}, {origin}-{destination}")
-        f.close()
-    await ctx.edit(content="Uploading to database.")
-    await ctx.edit(content="Uploading to database..")
-    await ctx.edit(content="Uploading to database...")
-    if aircraft == "B732":
-      embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015310203832516731/FJS_732_TwinJet_-_2022-09-02_13.20.01.png")
-    if aircraft == "B738":
-      embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015290133601320980/b738_4k_-_2022-08-29_16.09.22.PNG")
-    if aircraft == "A300":
-      embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015290004001542164/A300_P_V2_-_2022-08-31_00.37.05.PNG")
-    await ctx.edit(content=None, embed=embed)
-  else:
-    embed = discord.Embed(title="Error 403!", description="You do not have the <@&1013933799777783849> role. \nGet it in <#965686982304997466> before using this command!", color=errorc)
-    await ctx.respond(embed=embed)
-@va.command(name="flights", descripiton="Fetches flights a user has done.")
-async def datar(ctx, user: discord.Member = None):
-  guild = bot.get_guild(965419296937365514)
-  cfpilot = guild.get_role(1013933799777783849)
-  if cfpilot in ctx.author.roles:
-    if user == None:
-      author = ctx.author.id
-      await ctx.respond(f"loading your filed flights.")
-      sleep(0.5)
-      await ctx.edit(content=f"loading your filed flights..")
-      sleep(0.5)
-      await ctx.edit(content=f"loading your filed flights...")
-      if os.path.exists(f"ClearFly_VA/users/{author}"):
-          f = open(f"ClearFly_VA/users/{author}/data.txt","r")
-          with open(rf"ClearFly_VA/users/{author}/data.txt") as fp:
-              no = len(fp.readlines())
-              nof = no-1
-          datar = f.read()
-          embed = discord.Embed(title=f"Your Flights:", color=cfc, description=f"""
-          ```
-          {datar}
-          ```
-          Number of Flights: {nof}
-          """)
-          await ctx.edit(content=None,embed=embed)
+  if ctx.author == 668874138160594985:
+    guild = bot.get_guild(965419296937365514)
+    cfpilot = guild.get_role(1013933799777783849)
+    if cfpilot in ctx.author.roles:
+      dest = 1
+      ori = 1
+      if dest == 1:
+        if destination == "KDCA":
+          cf1 = 1
+        if destination == "KIAD":
+          cf1 = 2
+        if destination == "KLGA":
+          cf1 = 3
+        if destination == "KMSP":
+          cf1 = 4
+        if destination == "KORD":
+          cf1 = 5
+        if destination == "KMDW":
+          cf1 = 6
+        if destination == "KMKE":
+          cf1 = 7
+        if destination == "KSFO":
+          cf1 = 8
+        if destination == "KLAX":
+          cf1 = 9
+        if destination == "KPHX":
+          cf1 = 10
+        if destination == "KSEA":
+          cf1 = 11
+        if destination == "KPDX":
+          cf1 = 12
+        if destination == "KRIC":
+          cf1 = 13
+        if destination == "KMIA":
+          cf1 = 14
+        if destination == "KSTL":
+          cf1 = 15
+        if destination == "KBOS":
+          cf1 = 16
+        if destination == "KIND":
+          cf1 = 17
+        if destination == "KIAH":
+          cf1 = 18
+        if destination == "KAUS":
+          cf1 = 19
+        if destination == "KDFW":
+          cf1 = 20
+        if destination == "KPIT":
+          cf1 = 21
+        if destination == "KATL":
+          cf1 = 22
+        if destination == "KSAN":
+          cf1 = 23
+      if ori == 1:
+        if origin == "KDCA":
+          cf2 = 1
+        if origin == "KIAD":
+          cf2 = 2
+        if origin == "KLGA":
+          cf2 = 3
+        if origin == "KMSP":
+          cf2 = 4
+        if origin == "KORD":
+          cf2 = 5
+        if origin == "KMDW":
+          cf2 = 6
+        if origin == "KMKE":
+          cf2 = 7
+        if origin == "KSFO":
+          cf2 = 8
+        if origin == "KLAX":
+          cf2 = 9
+        if origin == "KPHX":
+          cf2 = 10
+        if origin == "KSEA":
+          cf2 = 11
+        if origin == "KPDX":
+          cf2 = 12
+        if origin == "KRIC":
+          cf2 = 13
+        if origin == "KMIA":
+          cf2 = 14
+        if origin == "KSTL":
+          cf2 = 15
+        if origin == "KBOS":
+          cf2 = 16
+        if origin == "KIND":
+          cf2 = 17
+        if origin == "KIAH":
+          cf2 = 18
+        if origin == "KAUS":
+          cf2 = 19
+        if origin == "KDFW":
+          cf2 = 20
+        if origin == "KPIT":
+          cf2 = 21
+        if origin == "KSAN":
+          cf2 = 22
+        if origin == "KATL":
+          cf2 = 22
+      user = ctx.author
+      await ctx.respond("Filing flight.")
+      sleep(0.1)
+      await ctx.edit(content="Filing flight..")
+      embed = discord.Embed(title="Flight Filed!", color=cfc)
+      flightnumber = cf1+cf2
+      embed.add_field(name="Your flight has been filed with the following data:", value=f"""
+      ```
+      Aircraft:{aircraft}
+      Departure:{origin}
+      Arrival:{destination}
+      Flight Number: CF{flightnumber}
+      ```
+      Have a nice and safe flight!
+                """)
+      await ctx.edit(content="Filing flight...")
+      await ctx.edit(content="Filing flight.")
+      if os.path.exists(f"ClearFly_VA/users/{user.id}"):
+          f = open(f"ClearFly_VA/users/{user.id}/data.txt","a")
+          f.write(f"\nCF{flightnumber}, {aircraft}, {origin}-{destination}")
           f.close()
       else:
-          embed = discord.Embed(title="Error 404!", description=f"No flights we're found for you, make sure you have flights filed!", color=errorc)
-          await ctx.edit(content=None, embed=embed)
+          os.mkdir(f"ClearFly_VA/users/{user.id}")
+          f = open(f"ClearFly_VA/users/{user.id}/data.txt","a")
+          f.write(f"\nCF{flightnumber}, {aircraft}, {origin}-{destination}")
+          f.close()
+      await ctx.edit(content="Uploading to database.")
+      await ctx.edit(content="Uploading to database..")
+      await ctx.edit(content="Uploading to database...")
+      if aircraft == "B732":
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015310203832516731/FJS_732_TwinJet_-_2022-09-02_13.20.01.png")
+      if aircraft == "B738":
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015290133601320980/b738_4k_-_2022-08-29_16.09.22.PNG")
+      if aircraft == "A300":
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1013239106198835300/1015290004001542164/A300_P_V2_-_2022-08-31_00.37.05.PNG")
+      await ctx.edit(content=None, embed=embed)
     else:
-          await ctx.respond(f"Loading {user}'s Filed Flights.")
-          sleep(0.5)
-          await ctx.edit(content=f"Loading {user}'s Filed flights..")
-          sleep(0.5)
-          await ctx.edit(content=f"Loading {user}'s Filed flights...")
-          if os.path.exists(f"ClearFly_VA/users/{user.id}"):
-              f = open(f"ClearFly_VA/users/{user.id}/data.txt","r")
-              with open(rf"ClearFly_VA/users/{user.id}/data.txt") as fp:
-                  no = len(fp.readlines())
-                  nof = no-1
-              datar = f.read()
-              embed = discord.Embed(title=f"{user}'s Flights:", color=cfc, description=f"""
-              ```
-              {datar}
-              ```
-              Number of Flights: {nof}
-              """)
-              await ctx.edit(content=None,embed=embed)
-              f.close()
-          else:
-              embed = discord.Embed(title="Error 404!", description=f"No flights we're found for {user.mention}, make sure they have flights filed!", color=errorc)
-              await ctx.edit(content=None, embed=embed)
+      embed = discord.Embed(title="Error 403!", description="You do not have the <@&1013933799777783849> role. \nGet it in <#965686982304997466> before using this command!", color=errorc)
+      await ctx.respond(embed=embed)
   else:
-    embed = discord.Embed(title="Error 403!", description="You do not have the <@&1013933799777783849> role. \nGet it in <#965686982304997466> before using this command!", color=errorc)
+    embed=discord.Embed(title="Error 503!", description="Most ClearFly VA commands are disable at the moment, read <#1013934267966967848> for more information.", color=cfc)
+    await ctx.respond(embed=embed)
+@va.command(name="flights", descripiton="Fetches flights a user has done.")
+async def flights(ctx, user: discord.Member = None):
+  if ctx.author == 668874138160594985:
+    guild = bot.get_guild(965419296937365514)
+    cfpilot = guild.get_role(1013933799777783849)
+    if cfpilot in ctx.author.roles:
+      if user == None:
+        author = ctx.author.id
+        await ctx.respond(f"loading your filed flights.")
+        sleep(0.5)
+        await ctx.edit(content=f"loading your filed flights..")
+        sleep(0.5)
+        await ctx.edit(content=f"loading your filed flights...")
+        if os.path.exists(f"ClearFly_VA/users/{author}"):
+            f = open(f"ClearFly_VA/users/{author}/data.txt","r")
+            with open(rf"ClearFly_VA/users/{author}/data.txt") as fp:
+                no = len(fp.readlines())
+                nof = no-1
+            datar = f.read()
+            embed = discord.Embed(title=f"Your Flights:", color=cfc, description=f"""
+            ```
+            {datar}
+            ```
+            Number of Flights: {nof}
+            """)
+            await ctx.edit(content=None,embed=embed)
+            f.close()
+        else:
+            embed = discord.Embed(title="Error 404!", description=f"No flights we're found for you, make sure you have flights filed!", color=errorc)
+            await ctx.edit(content=None, embed=embed)
+      else:
+            await ctx.respond(f"Loading {user}'s Filed Flights.")
+            sleep(0.5)
+            await ctx.edit(content=f"Loading {user}'s Filed flights..")
+            sleep(0.5)
+            await ctx.edit(content=f"Loading {user}'s Filed flights...")
+            if os.path.exists(f"ClearFly_VA/users/{user.id}"):
+                f = open(f"ClearFly_VA/users/{user.id}/data.txt","r")
+                with open(rf"ClearFly_VA/users/{user.id}/data.txt") as fp:
+                    no = len(fp.readlines())
+                    nof = no-1
+                datar = f.read()
+                embed = discord.Embed(title=f"{user}'s Flights:", color=cfc, description=f"""
+                ```
+                {datar}
+                ```
+                Number of Flights: {nof}
+                """)
+                await ctx.edit(content=None,embed=embed)
+                f.close()
+            else:
+                embed = discord.Embed(title="Error 404!", description=f"No flights we're found for {user.mention}, make sure they have flights filed!", color=errorc)
+                await ctx.edit(content=None, embed=embed)
+    else:
+      embed = discord.Embed(title="Error 403!", description="You do not have the <@&1013933799777783849> role. \nGet it in <#965686982304997466> before using this command!", color=errorc)
+      await ctx.respond(embed=embed)
+  else:
+    embed=discord.Embed(title="Error 503!", description="Most ClearFly VA commands are disable at the moment, read <#1013934267966967848> for more information.", color=errorc)
     await ctx.respond(embed=embed)
 
+@va.command(name="overview", description="Get an overview over all flights in the va.")
+async def overview(ctx):
+    embed=discord.Embed(title="Error 503!", description="Most ClearFly VA commands are disable at the moment, read <#1013934267966967848> for more information.", color=errorc)
+    await ctx.respond(embed=embed)
+
+@va.command(name="liveries", description="Looking to fly for the ClearFly VA? Here are the liveries to get you started!")
+async def valivs(ctx):
+  embed=discord.Embed(title="ClearFly VA Official Liveries:", description="[Boeing 737-800 by Zibo](https://drive.google.com/drive/u/1/folders/1DEzn_jPgyME-U1FrUs3eX4QTwsgwbfpD)\n[Airbus A300-600 by IniSimulations](https://drive.google.com/drive/u/1/folders/16n0cnwkTeGWBhUQJZhXtNz4oq4n4Pe86)\n[Boeing 737-200 by FlyJSim](https://drive.google.com/drive/u/1/folders/1g-vZsECHyHQMbjwnasxHwj0TXjxfLQ0P)")
+  await ctx.respond(embed)
 
 
 ###############
@@ -888,7 +886,9 @@ async def embed(ctx):
             value=f"""
 ```
 /va file : File a flight you are gonna do for the ClearFly VA.
-/va flights : Fetches information about all flights a user has done
+/va flights : Fetches information about all flights a user has done.
+/va overview : Get an overview over all flights in the va.
+/va liveries : Get all liveries to get your journey started.
 ```
             """, inline=False
       )
