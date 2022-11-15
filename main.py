441fbe89487f411c12350643d784a106752b5fb5
Matt3o0 <duvbolone@gmail.com>
2022-11-15 21:33:25 +0100

    plez workkkk

--- main.py
+++ main.py
@@ -853,7 +853,7 @@ async def metar(ctx, icao):
   resp = json.loads(req.text)
   if resp['results'] == 1:
     time = str(json.dumps(resp['data'][0]['observed']).replace('"', ""))
-    obstime = discord.utils.format_dt(datetime.fromisoformat(time), "R")
+    obstime = datetime.fromisoformat(time)
     airportn = json.dumps(resp['data'][0]['station']['name']).replace("'", "")
     embed = discord.Embed(title=f"Metar data for {airportn} from {time}({obstime})", color=cfc)
     embed.add_field(name="Raw Metar Data:", value=f"""
