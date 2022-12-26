30095da324635f57b32ca8278776f036058f65d2
Matt3o0 <83077061+duvbolone@users.noreply.github.com>
2022-12-26 19:03:46 +0100

    Update listeners.py

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -130,10 +130,10 @@ class Listeners(discord.Cog):
             if before.roles != after.roles:
                 embed = discord.Embed(title=f"{before} got their roles changed.", colour=cfc)
                 brole = [str(role.id) for role in before.roles]
-                brole = ["<@&" + str(role) for str(role) in brole]
+                brole = ["<@&" + str(role) for role in brole]
                 brole = ">\n".join(brole)
                 arole = [str(role.id) for role in after.roles]
-                brole = ["<@&" + str(role) for str(role) in arole]
+                brole = ["<@&" + str(role) for role in arole]
                 arole = ">\n".join(arole)
                 embed.add_field(name="Roles before", value=brole)
                 embed.add_field(name="Roles after", value=arole)
