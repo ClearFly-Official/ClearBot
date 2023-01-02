50d2b871294fbe29f951a40c445f6f958ef5037a
Matt3o0 <duvbolone@gmail.com>
2023-01-02 01:47:14 +0100

    better EH

--- cogs/aviation.py
+++ cogs/aviation.py
@@ -109,7 +109,7 @@ Winds : **{json.dumps(resp['data'][0].get('wind', {'degrees':'N/A'}).get('degree
             """, inline=False)
             await ctx.respond(embed=embed, view=METARViewI(bot=self.bot))
         else:
-            embed = discord.Embed(title="Error 404!", description="Didn't found metar data for that airport.", color=errorc)
+            embed = discord.Embed(title="Error 404!", description=f"Didn't found metar data for {airport[:4].upper()}.", color=errorc)
             await ctx.respond(embed=embed)
 
     @av.command(name="airport-diagram", description="Fetches the airport diagram of the provided airport.")
@@ -120,7 +120,7 @@ Winds : **{json.dumps(resp['data'][0].get('wind', {'degrees':'N/A'}).get('degree
             req = requests.get(f"https://api.aviationapi.com/v1/charts?apt={airport[:4].upper()}&group=2")
             load = json.loads(req.text)
             if load[airport[:4].upper()] == []:
-                embed = discord.Embed(title="Error 404", description="I didn't found that airport.", colour=errorc)
+                embed = discord.Embed(title="Error 404", description="Didn't found a diagram for {airport[:4].upper()}.", colour=errorc)
                 await ctx.respond(embed=embed)
             else:
                 url = load[airport[:4].upper()][0]['pdf_path']
