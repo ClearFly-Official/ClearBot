890ea1cd07596c019029ebfcc5d3e5d97c68db3c
Matt3o0 <duvbolone@gmail.com>
2023-02-28 22:36:31 +0100

    Fix errrors again

--- cogs/fun.py
+++ cogs/fun.py
@@ -494,7 +494,7 @@ class FunCommands(discord.Cog):
             else:
                 random.seed(time.time())
                 subm = random.choice(subms)
-                self.bot.shown_rsubms.append(subm.id)
+                self.bot.shownrsubms.append(subm.id)
                 embed = discord.Embed(
                     title=subm.title,
                     url="https://reddit.com" + subm.permalink,
